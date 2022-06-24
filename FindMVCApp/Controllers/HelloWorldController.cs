817a373b54f3905e17c852d05bf957759f61ba79
Steve BERENGER <steve.berenger@gmail.com>
2022-06-24 11:48:27 +0200

    create

--- FindMVCApp/Controllers/HelloWorldController.cs
+++ FindMVCApp/Controllers/HelloWorldController.cs
@@ -2,23 +2,30 @@
 using FirstMVCApp.Models;
 
 
+
 namespace FirstMVCApp.Controllers
 {
     public class HelloWorldController : Controller
     {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
         public IActionResult Index()
         {
-            DogViewModel doggo = new DogViewModel() 
-            { Name ="Sif" , Age = 2};
-            return View(doggo);
+           
+            return View(dogs);
         }
 
         public IActionResult Create()
         {
-            return View(); 
+            var dogVm = new DogViewModel();
+            return View(dogVm); 
         }
 
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
 
+        }
 
         public string Hello()
         {
